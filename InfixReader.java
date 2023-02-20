13cab3b4faf20f7ad976a5afb471e59866d47c87
22046-NelsonEscalante <esc22046@uvg.edu.gt>
2023-02-20 11:25:35 -0600

    Added the infixReader

---
+++ InfixReader.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+
+public class InfixReader {
+    
+    public String toPostfix(String s) {
+        String[] unordered = s.split("");
+        AbstractStack<String> sorting = new StackArrayList<>();
+        AbstractStack<String> temp = new StackArrayList<>();
+        ArrayList<String> ordered = new ArrayList<>();
+        String result;
+        for (String c : unordered) {
+            if (checkNum(c)) {
+                sorting.push(c);
+            } else {
+                temp.push(c);
+            }
+        }
+
+        while (!temp.isEmpty()) {
+            sorting.push(temp.pull());
+        }
+
+        while (!sorting.isEmpty()) {
+            temp.push(sorting.pull());
+        }
+
+        while (!temp.isEmpty()) {
+            ordered.add(temp.pull());
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (String x : ordered) {
+            sb.append(x);
+        }
+
+        result = sb.toString();
+
+        return result;
+    }
+
+    private boolean checkNum(String s) {
+        try {
+            Integer.parseInt(s);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
